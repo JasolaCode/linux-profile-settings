7b719f72a666297a75f9254f50c83398cc8f9519
JasolaCode <joshsolano56@gmail.com>
2023-10-26 09:48:53 +1100

    IT WORKSgit add * The git branch in bash prompt works perefcetlygit add *

--- install.sh
+++ install.sh
@@ -4,7 +4,8 @@ HMDIR=$(echo ~)
 FOLDER=".vimbash$(date +%s)"
 
 mkdir "${HMDIR}/${FOLDER}"
-echo "Moving files into ${HMDIR}..."
+echo "About to move files into ${HMDIR}..."
+echo "**Expect more output on success**"
 
 # just a simple copy of existing files, files won't be overwritten unless the first copy works
-touch ${HMDIR}/.vimrc ${HMDIR}/.bashrc ${HMDIR}/.bash_profile ${HMDIR}/.bash_aliases && cp ${HMDIR}/.vimrc ${HMDIR}/.bashrc ${HMDIR}/.bash_profile ${HMDIR}/.bash_aliases ${HMDIR}/${FOLDER} && cp .vimrc .bashrc .bash_profile .bash_aliases ${HMDIR} && echo "Successfully copied files..." 
+touch ${HMDIR}/.vimrc ${HMDIR}/.bashrc ${HMDIR}/.bash_profile ${HMDIR}/.bash_aliases ${HMDIR}/.git-prompt.sh && cp ${HMDIR}/.vimrc ${HMDIR}/.bashrc ${HMDIR}/.bash_profile ${HMDIR}/.bash_aliases ${HMDIR}/.git-prompt.sh ${HMDIR}/${FOLDER} && cp .vimrc .bashrc .bash_profile .bash_aliases .git-prompt.sh ${HMDIR} && echo "Successfully copied files..." 
