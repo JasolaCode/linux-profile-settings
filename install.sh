113907a9290e904363e359243c3e8537f6047233
JasolaCode <joshsolano56@gmail.com>
2023-10-26 12:01:43 +1100

    adjusted install.sh for ssh config

--- install.sh
+++ install.sh
@@ -8,4 +8,4 @@ echo "About to move files into ${HMDIR}..."
 echo "**Expect more output on success**"
 
 # just a simple copy of existing files, files won't be overwritten unless the first copy works
-touch ${HMDIR}/.vimrc ${HMDIR}/.bashrc ${HMDIR}/.bash_profile ${HMDIR}/.bash_aliases ${HMDIR}/.profile ${HMDIR}/.git-prompt.sh && cp ${HMDIR}/.vimrc ${HMDIR}/.bashrc ${HMDIR}/.bash_profile ${HMDIR}/.bash_aliases ${HMDIR}/.profile ${HMDIR}/.git-prompt.sh ${HMDIR}/${FOLDER} && cp .vimrc .bashrc .bash_profile .bash_aliases .profile .git-prompt.sh ${HMDIR} && echo "Successfully copied files..." 
+touch ${HMDIR}/.vimrc ${HMDIR}/.bashrc ${HMDIR}/.bash_profile ${HMDIR}/.bash_aliases ${HMDIR}/.profile ${HMDIR}/.git-prompt.sh && cp ${HMDIR}/.vimrc ${HMDIR}/.bashrc ${HMDIR}/.bash_profile ${HMDIR}/.bash_aliases ${HMDIR}/.profile ${HMDIR}/.git-prompt.sh ${HMDIR}/${FOLDER} && cp .vimrc .bashrc .bash_profile .bash_aliases .profile .git-prompt.sh ${HMDIR} && cp .ssh-config ${HMDIR}/.ssh/config && echo "Successfully copied files..." 
